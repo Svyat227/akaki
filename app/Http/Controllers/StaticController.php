0b384b9bd58ccafc433d5a5e749da43f0a32b458
svyat <danilov.svat@mail.ru>
2020-10-09 12:56:55 +0300

    first message

---
+++ app/Http/Controllers/StaticController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticController extends Controller
+{
+    public function getIndex ($url = null)
+	{
+		return view ('static');
+	}
+}
